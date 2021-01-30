843ec7dae1669c027beb0cd782a9895fd2d0a080
Vladimir Kochetkov <kochetkov2209@yandex.ru>
2021-01-30 14:45:01 +0300

    Первое задание первого урока

--- script/script.js
+++ script/script.js
@@ -1,9 +1,9 @@
 
-    let money = "One";
-    let income = 2;
-    let addExpenses = 3;
-    let deposit = 4;
-    let mission = 5;
+    let money = 50;
+    let income = 'фриланс';
+    let addExpenses = 'интернет, такси, коммуналка';
+    let deposit = true;
+    let mission = 200;
     let period = 6;
     alert("Первый урок");
     console.log("Glo academy");
